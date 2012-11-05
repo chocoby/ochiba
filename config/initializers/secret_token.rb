dcc3d76e0c13459b8c29d2990dcfe52769c65eac
chocoby <chocoby@gmail.com>
2012-11-05 15:09:11 +0900

    initial commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ochiba::Application.config.secret_token = '4ef4ce00fc3bd91d99f1978f28725491d1897a9716508d1165a643c739882ea994376c9f27850d97efe7c767a85614f68555fa5fe608af05eacfbbe5aaad5c40'
