c4baf718f62057da6f6407b4bb5f5bc2cc8035b3
chocoby <chocoby@gmail.com>
2012-11-06 00:39:32 +0900

    simple login form... #1
    MUST USE HTTPS...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :current_user, :signed_in?
+
+  def current_user
+    @current_user = session[:username]
+  end
+
+  def current_user=(username)
+    session[:username] = username
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  def sign_out
+    session.delete(:username)
+  end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+# coding: utf-8
+class SessionsController < ApplicationController
+  # GET /login
+  def new
+    self.current_user
+  end
+
+  # POST /login
+  def create
+    auth = Ochiba::Auth.authenticate(params[:username], params[:password])
+    return redirect_to new_user_session_path unless auth
+
+    self.current_user = params[:username]
+
+    render text: 'ログインしました！'
+  end
+
+  # DELETE /logout
+  def destroy
+    self.sign_out
+    redirect_to new_user_session_path
+  end
+end
