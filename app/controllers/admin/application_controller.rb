a51ec49bb52cd3db5c2f011672c296a8fe6c882a
chocoby <chocoby@gmail.com>
2012-11-07 01:13:24 +0900

    admin home by bootstrap

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,6 +1,8 @@
 class Admin::ApplicationController < ApplicationController
   before_filter :authenticate_user!
 
+  layout 'admin'
+
   # ログインを必須とする
   def authenticate_user!
     return redirect_to new_user_session_path unless signed_in?
--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,6 +1,5 @@
 class Admin::HomeController < Admin::ApplicationController
   # GET /admin
   def index
-    render text: 'test'
   end
 end
