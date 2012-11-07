265675e11a9b118aa486592f68e69564c3c9edd2
chocoby <chocoby@gmail.com>
2012-11-07 09:52:53 +0900

    rake db:migrate

--- db/migrate/20121107005112_create_manufacturers.rb
+++ db/migrate/20121107005112_create_manufacturers.rb
@@ -1,7 +1,7 @@
 class CreateManufacturers < ActiveRecord::Migration
   def change
     create_table :manufacturers do |t|
-      t.string :name
+      t.string :name, null: false, limit: 50
 
       t.timestamps
     end
