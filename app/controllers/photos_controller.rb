7907041bfd0b00169e8bba75aeb06bbaedf87705
chocoby <chocoby@gmail.com>
2012-11-13 10:14:44 +0900

    install paginator in photostream

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,9 @@
 # coding: utf-8
 class PhotosController < ApplicationController
+  PHOTOSTREAM_PAGE_PER = 10
+
   # GET /
   def index
-    @photos = Photo.new_uploads.all
+    @photos = Photo.new_uploads.page(params[:page]).per(PHOTOSTREAM_PAGE_PER)
   end
 end
