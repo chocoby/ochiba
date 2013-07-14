b0e18efcfd102b98c7532d2c69c1f5f0bbac24b3
chocoby <chocoby@gmail.com>
2013-07-14 12:39:56 +0900

    rake rails:update

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'active_record/railtie'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'sprockets/railtie'
+require 'rails/all'
 
-# Assets should be precompiled for production (so we don't need the gems loaded then)
-Bundler.require(*Rails.groups(assets: %w(development test)))
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(:default, Rails.env)
 
 module Ochiba
   class Application < Rails::Application
@@ -14,13 +12,9 @@ module Ochiba
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/lib)
-
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-    config.time_zone = 'Tokyo'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 
 # Make sure the secret is at least 30 characters and all random,
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Ochiba::Application.config.secret_key_base = 'b5435cc1253529800ee5d4e6502c0cc609323de741864783dccf1526fccbbd2315770c3e056398243a59eee4e4589cc662d984fe653b22f92563e9494fbc7fed'
+Ochiba::Application.config.secret_key_base = '94a7fc3ed1228a850043a547bea7c13b380df8a8799dc230a2bcacb3f655295825213450e388edf5e39f58046af190ad3381c98c4b1ba24eaa0728cf04ea114e'
