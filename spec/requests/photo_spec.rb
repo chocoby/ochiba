b3de2b86407b21e9500583e45c14227b818f531d
chocoby <chocoby@gmail.com>
2014-01-04 21:03:37 +0900

    Add request spec

--- spec/requests/photo_spec.rb
+++ spec/requests/photo_spec.rb
@@ -5,4 +5,10 @@ describe "Photos" do
     before { visit root_path }
     it { expect(page.status_code).to be 200 }
   end
+
+  describe 'Show' do
+    let(:photo) { FactoryGirl.create(:photo) }
+    before { visit photo_path(photo) }
+    it { expect(page.status_code).to be 200 }
+  end
 end
